dcf455d5030a3564b46111a5bfc13dfb9045effa
Umer Kazi <umerkazi@hotmail.com>
2021-01-12 20:33:56 -0500

    v1

--- server.js
+++ server.js
@@ -31,9 +31,7 @@ mongoose.connect(URI, {
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-    app.get('*', (req, res) =>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'App.js'))
-    });
+    
 }
 
 // Listen Server
